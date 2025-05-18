f29527ddcf5ae669b3b0b89c39c278dfca62eb81
ScruffyMeow <hypnos@cloud.com>
2025-05-18 17:02:04 +0800

    internal/model complete. next one is internal/service

--- internal/model/models.go
+++ internal/model/models.go
@@ -1,1 +1,9 @@
 package model
+
+import (
+	_ "goAccounting/internal/model/category"
+	_ "goAccounting/internal/model/comment"
+	_ "goAccounting/internal/model/common"
+	_ "goAccounting/internal/model/transaction"
+	_ "goAccounting/internal/model/user"
+)
