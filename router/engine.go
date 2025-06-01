5c1f16a2c9c744bc995aca68e2259489db59cd39
littla2liyou <kafbcc@163.com>
2025-06-01 12:59:12 +0800

    更新cors跨域

--- router/engine.go
+++ router/engine.go
@@ -3,6 +3,7 @@ package router
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,8 @@ var Engine *gin.Engine
 func init() {
 	fmt.Println("router.engine: start init engine")
 	Engine = gin.Default()
+	// 添加 CORS 中间件
+	Engine.Use(cors.Default())
 	if Engine == nil {
 		fmt.Println("router.engine: engine is NIL")
 	} else {
