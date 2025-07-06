474556019670fbbec2d01f7d567a123154baa1ee
√Y <2795482865@qq.com>
2025-07-06 20:26:32 +0800

    engine.go CORS prob

--- router/engine.go
+++ router/engine.go
@@ -17,9 +17,24 @@ func init() {
 		AllowOrigins:  []string{"*"},
 		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders: []string{"Content-Length"},
-		//AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: false, 
+		MaxAge:           12 * 60 * 60, 
 	}))
+
+
+	Engine.Use(func(c *gin.Context) {
+		// 处理预检请求
+		if c.Request.Method == "OPTIONS" {
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
+			c.Header("Access-Control-Allow-Headers", "Origin, Content-Length, Content-Type, Authorization, Accept, X-Requested-With")
+			c.Header("Access-Control-Max-Age", "43200") // 12小时
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 
 	// 添加测试路由，不需要认证
 	Engine.GET("/test", func(ctx *gin.Context) {
